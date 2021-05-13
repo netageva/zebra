fccfa3e233972ae21d0521feed1a46165229b979
Neta Geva <ngeva91@gmail.com>
2021-05-13 19:42:41 +0300

    mlm scoring final model and app

--- mlm_scoring_heBERT.py
+++ mlm_scoring_heBERT.py
@@ -3,7 +3,8 @@ from torch.nn import functional as F
 import numpy as np
 import config as CFG
 
-class BertMlmScoring():
+
+class BertMlmScoring:
     def __init__(self):
         super(BertMlmScoring, self).__init__()
         self.tokenizer = AutoTokenizer.from_pretrained(CFG.HE_BERT)
@@ -13,53 +14,15 @@ class BertMlmScoring():
     def calc_score_for_sent(self, text):
         token_text = self.tokenizer.encode_plus(text, add_special_tokens=True, return_tensors="pt")
         self.sent_score = 0
-        for i in range(len(token_text[CFG.IDS_KEY][0])):
+        for i in range(len(token_text[CFG.IDS_KEY][0])):  # for each word in tokenized text
             token_text = self.tokenizer.encode_plus(text, add_special_tokens=True, return_tensors="pt")
-            token_text[CFG.IDS_KEY][0][i] = self.tokenizer.mask_token_id
+            token_text[CFG.IDS_KEY][0][i] = self.tokenizer.mask_token_id  # add MASK token in the ith index
             word_index = token_text[CFG.IDS_KEY][0][i]
-            output = self.model(**token_text)
-            logits = output.logits
+            output = self.model(**token_text)  # BERT model
+            logits = output.logits  # embedding layer (scores in dictionary)
             softmax = F.softmax(logits, dim=-1)
             mask_word = softmax[0, i, :]
             word_score = np.log((mask_word[word_index]).detach().numpy())
             self.sent_score += word_score
-            self.sent_score = self.sent_score/len(token_text[CFG.IDS_KEY][0])
+            self.sent_score = self.sent_score/len(token_text[CFG.IDS_KEY][0])  # normalize by sentence length
         return self.sent_score
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-#prediction_logits
-#take bert embedding layer and multiple it with y(dim=3)
-m = nn.Softmax(dim=3)
-proba = m(y)
-(1,512,50000) - log on dim 3
-print(proba.shape)
-proba = proba.detach().numpy()
-log_proba = np.log(proba)
-sum_log = np.sum(log_proba)
-print('score:',sum_log)'''
